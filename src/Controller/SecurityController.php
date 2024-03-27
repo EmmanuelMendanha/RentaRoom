46c732fb7b3d0f6d1f607c5317e5cf7bce298d4c
EmmanuelMendanha <156678400+EmmanuelMendanha@users.noreply.github.com>
2024-03-27 11:42:48 +0100

    ajout de la home page ainsi que la securisation du login pour redigerer le user sur la page des rooms

--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -3,6 +3,7 @@
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Bundle\SecurityBundle\Security;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
@@ -10,11 +11,19 @@ use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
 class SecurityController extends AbstractController
 {
     #[Route(path: '/login', name: 'app_login')]
-    public function login(AuthenticationUtils $authenticationUtils): Response
+    public function login(AuthenticationUtils $authenticationUtils, Security $security): Response
     {
-        // if ($this->getUser()) {
-        //     return $this->redirectToRoute('target_path');
-        // }
+          // Vérifie si l'utilisateur est déjà connecté
+            {
+                if ($security->getUser() !== null) {
+                    // Redirige vers la page appropriée en fonction du rôle de l'utilisateur
+                    if (in_array('ROLE_USER', $security->getUser()->getRoles(), true)) {
+                    return $this->redirectToRoute('rooms');
+                } else {
+                    // Redirige vers une autre page selon le rôle, ou vers une page par défaut
+                    return $this->redirectToRoute('dashboard');
+                }
+            }
 
         // get the login error if there is one
         $error = $authenticationUtils->getLastAuthenticationError();
@@ -22,6 +31,7 @@ class SecurityController extends AbstractController
         $lastUsername = $authenticationUtils->getLastUsername();
 
         return $this->render('security/login.html.twig', ['last_username' => $lastUsername, 'error' => $error]);
+    }
     }
 
     #[Route(path: '/logout', name: 'app_logout')]
