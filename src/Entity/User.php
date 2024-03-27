e5dcce4fa003b92da669ec14b300f5c1d1ef795c
EmmanuelMendanha <156678400+EmmanuelMendanha@users.noreply.github.com>
2024-03-27 12:24:40 +0100

    ajout du bundle pour les email ainsi que du formulaire de creation de compte

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -4,11 +4,13 @@ namespace App\Entity;
 
 use App\Repository\UserRepository;
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Bridge\Doctrine\Validator\Constraints\UniqueEntity;
 use Symfony\Component\Security\Core\User\PasswordAuthenticatedUserInterface;
 use Symfony\Component\Security\Core\User\UserInterface;
 
 #[ORM\Entity(repositoryClass: UserRepository::class)]
 #[ORM\UniqueConstraint(name: 'UNIQ_IDENTIFIER_EMAIL', fields: ['email'])]
+#[UniqueEntity(fields: ['email'], message: 'There is already an account with this email')]
 class User implements UserInterface, PasswordAuthenticatedUserInterface
 {
     #[ORM\Id]
@@ -42,6 +44,9 @@ class User implements UserInterface, PasswordAuthenticatedUserInterface
 
     #[ORM\ManyToOne(inversedBy: 'users')]
     private ?Booking $booking_users = null;
+
+    #[ORM\Column(type: 'boolean')]
+    private $isVerified = false;
 
 
     public function getId(): ?int
@@ -163,6 +168,18 @@ class User implements UserInterface, PasswordAuthenticatedUserInterface
     public function setBookingUsers(?Booking $booking_users): static
     {
         $this->booking_users = $booking_users;
+
+        return $this;
+    }
+
+    public function isVerified(): bool
+    {
+        return $this->isVerified;
+    }
+
+    public function setIsVerified(bool $isVerified): static
+    {
+        $this->isVerified = $isVerified;
 
         return $this;
     }
