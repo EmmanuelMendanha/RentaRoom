f8ff990b0071889f8e95885ac59c29875eb02010
ClemMaz <clementmazurkiewicz.dev@gmail.com>
2024-03-27 14:26:03 +0100

    ajout fonction bookRoom

--- src/Controller/RoomsController.php
+++ src/Controller/RoomsController.php
@@ -2,10 +2,12 @@
 
 namespace App\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use App\Entity\Booking;
+use App\Repository\RoomRepository;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
-use App\Repository\RoomRepository;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class RoomsController extends AbstractController
 {
@@ -28,5 +30,27 @@ class RoomsController extends AbstractController
             'room' => $room,
         ]);
     }
+
+    #[Route('/rooms/{id}/book', name: 'room_book')]
+    public function bookRoom(RoomRepository $roomRepository, $id, EntityManagerInterface $em): Response
+    {
+        $room = $roomRepository->find($id);
+
+        
+        $booking = new Booking();
+        $booking->getRooms($room);
+        $booking->getUsers($this->getUser()); // Assuming you have a logged in user.
+        $booking->setDateIn(new \DateTime()); // Set this to the desired start date.
+        $booking->setDateOut(new \DateTime()); // Set this to the desired end date.
+
+        $em->persist($booking);
+        $em->flush();
+
+        return $this->redirectToRoute('room_show', ['id' => $id]);
+
+
+    }
+
+
 }
 
