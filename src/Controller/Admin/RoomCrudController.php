93c9ee5167f3bee77fdba2818dcbc71ae4f3a691
EmmanuelMendanha <156678400+EmmanuelMendanha@users.noreply.github.com>
2024-03-29 12:13:00 +0100

    Update EasyAdminBundle configuration

--- src/Controller/Admin/RoomCrudController.php
+++ src/Controller/Admin/RoomCrudController.php
@@ -3,10 +3,13 @@
 namespace App\Controller\Admin;
 
 use App\Entity\Room;
-use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
+use EasyCorp\Bundle\EasyAdminBundle\Config\Crud;
+use EasyCorp\Bundle\EasyAdminBundle\Config\Action;
 use EasyCorp\Bundle\EasyAdminBundle\Field\IdField;
-use EasyCorp\Bundle\EasyAdminBundle\Field\TextEditorField;
+use EasyCorp\Bundle\EasyAdminBundle\Config\Actions;
 use EasyCorp\Bundle\EasyAdminBundle\Field\TextField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\TextEditorField;
+use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
 
 class RoomCrudController extends AbstractCrudController
 {
@@ -14,7 +17,21 @@ class RoomCrudController extends AbstractCrudController
     {
         return Room::class;
     }
+    public function configureCrud(Crud $crud): Crud
+    {
+        return $crud
+            ->setPageTitle('index', '📆 All rooms')
+            ->setEntityLabelInSingular('Room')
+            ->setEntityLabelInPlural('Rooms')
+            ->setSearchFields(['name'])
+            ->setDefaultSort(['id' => 'DESC']);
+    }
 
+    public function configureActions(Actions $actions): Actions
+    {
+        return $actions
+            ->add(Crud::PAGE_INDEX, Action::DETAIL);
+    }
     /*
     public function configureFields(string $pageName): iterable
     {
--- src/Controller/Admin/SoftwareCrudController.php
+++ src/Controller/Admin/SoftwareCrudController.php
@@ -3,16 +3,35 @@
 namespace App\Controller\Admin;
 
 use App\Entity\Software;
-use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
+use EasyCorp\Bundle\EasyAdminBundle\Config\Crud;
+use EasyCorp\Bundle\EasyAdminBundle\Config\Action;
 use EasyCorp\Bundle\EasyAdminBundle\Field\IdField;
-use EasyCorp\Bundle\EasyAdminBundle\Field\TextEditorField;
+use EasyCorp\Bundle\EasyAdminBundle\Config\Actions;
 use EasyCorp\Bundle\EasyAdminBundle\Field\TextField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\TextEditorField;
+use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
 
 class SoftwareCrudController extends AbstractCrudController
 {
     public static function getEntityFqcn(): string
     {
         return Software::class;
+    }
+
+    public function configureCrud(Crud $crud): Crud
+    {
+        return $crud
+            ->setPageTitle('index', '📆 All softwares')
+            ->setEntityLabelInSingular('Software')
+            ->setEntityLabelInPlural('Softwares')
+            ->setSearchFields(['name'])
+            ->setDefaultSort(['id' => 'DESC']);
+    }
+
+    public function configureActions(Actions $actions): Actions
+    {
+        return $actions
+            ->add(Crud::PAGE_INDEX, Action::DETAIL);
     }
 
     /*
